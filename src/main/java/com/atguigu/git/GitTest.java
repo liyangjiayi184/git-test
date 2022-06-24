190517b9dc7f8813d8044116596657e652000e98
LiYangJiaYi <liyangjiayi184@163.com>
2022-06-24 16:45:18 +0800

    hot-fix first commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("Hello World!!1");
         System.out.println("Hello World!!2");
         System.out.println("Hello World!!3");
+        System.out.println("Hello World!!4");
     }
 }
